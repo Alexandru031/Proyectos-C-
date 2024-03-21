14f40134e0e3b255bba9bab57cd95f51a616a2a6
Programacion <programacion@edein.com>
2024-03-21 11:55:58 +0100

    Añade las primeras pruebas

---
+++ PruebaAppWindows2/Ventas.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace PruebaAppWindows2
+{
+    partial class Ventas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.lbTitulo = new System.Windows.Forms.Label();
+            this.txtEscribir = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEjecutar.Location = new System.Drawing.Point(208, 202);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(153, 57);
+            this.btnEjecutar.TabIndex = 0;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // lbTitulo
+            // 
+            this.lbTitulo.AutoSize = true;
+            this.lbTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitulo.Location = new System.Drawing.Point(205, 154);
+            this.lbTitulo.Name = "lbTitulo";
+            this.lbTitulo.Size = new System.Drawing.Size(81, 13);
+            this.lbTitulo.TabIndex = 1;
+            this.lbTitulo.Text = "Escriba aqui:";
+            this.lbTitulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtEscribir
+            // 
+            this.txtEscribir.BackColor = System.Drawing.SystemColors.InfoText;
+            this.txtEscribir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEscribir.ForeColor = System.Drawing.SystemColors.Info;
+            this.txtEscribir.Location = new System.Drawing.Point(208, 170);
+            this.txtEscribir.Name = "txtEscribir";
+            this.txtEscribir.Size = new System.Drawing.Size(264, 26);
+            this.txtEscribir.TabIndex = 2;
+            this.txtEscribir.Text = "Hola Mundo";
+            // 
+            // Ventas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtEscribir);
+            this.Controls.Add(this.lbTitulo);
+            this.Controls.Add(this.btnEjecutar);
+            this.Name = "Ventas";
+            this.Text = "Formulario editado";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.Label lbTitulo;
+        private System.Windows.Forms.TextBox txtEscribir;
+    }
+}
+
---
+++ PruebaAppWindows2/Ventas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaAppWindows2
+{
+    public partial class Ventas : Form
+    {
+        public Ventas()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            txtEscribir.Text = "Texto cambiado";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtEscribir.Text = "Cargar";
+        }
+    }
+}
